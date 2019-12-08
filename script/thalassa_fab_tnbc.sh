e3a6806f59b68f4d5ec3cd6fae31fe90b8422248
pnaylor <peter.naylor@mines-paristech.fr>
2019-12-08 16:07:54 +0100

    first nexflox process

--- script/thalassa_fab_tnbc.sh
+++
@@ -1,6 +0,0 @@
-nextflow run nextflow/CellularSegmentation.nf -resume -c ~/.nextflow/config -profile mines \
-                                           --PROJECT_NAME fab_tnbc --PROJECT_VERSION 1-0 \
-                                           --tiff_location ../Data/Biopsy \
-                                           --tissue_bound_annot ../Data/Biopsy/tissue_segmentation \
-                                           --segmentation_weights ../test_judith_project/tmp/test_tcga_project/model/Distance111008_32 \
-                                           --segmentation_mean ../test_judith_project/tmp/test_tcga_project/model/mean_file_111008.npy
---
+++ script/thalassa_tiling.sh
@@ -0,0 +1,3 @@
+nextflow run nextflow/Tiling-encoding.nf -resume -c ~/.nextflow/config -profile home \
+                                         --tiff_location ../Data/Combined_Biopsy \
+                                         --tissue_bound_annot ../Data/Combined_Biopsy/combined_tissue_segmentation
