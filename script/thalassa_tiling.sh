2a35f4e43e563a85c721a415812527577369745d
pnaylor <peter.naylor@mines-paristech.fr>
2019-12-08 16:08:47 +0100

    first nexflox process

--- script/thalassa_tiling.sh
+++ script/thalassa_tiling.sh
@@ -1,3 +1,3 @@
-nextflow run nextflow/Tiling-encoding.nf -resume -c ~/.nextflow/config -profile home \
+nextflow run nextflow/Tiling-encoding.nf -resume -c ~/.nextflow/config -profile mines \
                                          --tiff_location ../Data/Combined_Biopsy \
                                          --tissue_bound_annot ../Data/Combined_Biopsy/combined_tissue_segmentation
