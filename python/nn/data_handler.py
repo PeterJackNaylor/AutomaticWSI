5572b366e61e8d432954d154543e67edd908ba4d
PeterJackNaylor <peter.jack.naylor@gmail.com>
2022-02-08 22:55:59 +0900

    rdm commit

--- python/nn/data_handler.py
+++ python/nn/data_handler.py
@@ -439,6 +439,7 @@ class h5_Sequencer_HL(Sequence):
             new_y.append(np.tile(self.y_onehot[i], (end-start,1)))
 
         self.new_indices = np.concatenate(new_indices)
+        import pdb; pdb.set_trace()
         self.new_x = self.data[new_indices]
 
         self.new_y = np.vstack(new_y)
